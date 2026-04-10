ee84f7155945e39c7a5bfc71ff524cad7f30b258
MD. Jahidul Islam Jony <jahiduljony60@gmail.com>
2026-04-10 19:13:28 +0600

    Add files via upload

---
+++ If Else.py
@@ -0,0 +1,9 @@
+
+a = int(input("Enter the value of A: "))
+b = int(input("Enter the value of B: "))
+
+if (a>b) :
+        print("A is greater than B.")
+else :
+        print("B is greater than A.")
+
