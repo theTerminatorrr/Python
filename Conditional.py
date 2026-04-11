200c1d39e8a906c9aa27637f934b3c79868a984c
MD. Jahidul Islam Jony <jahiduljony60@gmail.com>
2026-04-12 00:28:36 +0600

    Add files via upload

---
+++ Conditional.py
@@ -0,0 +1,18 @@
+x = 20
+if x % 2 == 0 :
+  print('Even')
+  x += 2
+else :
+  print ('Odd')
+
+print (x)
+
+
+
+marks = 70
+if marks > 90 :
+  print ('A')
+elif marks > 86 :
+  print ('B')
+else :
+  print ('C')
