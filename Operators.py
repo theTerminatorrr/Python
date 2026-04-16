a44d3be3424f1ed58b88b3731466a4aeebbb5af7
MD. Jahidul Islam Jony <jahiduljony60@gmail.com>
2026-04-16 14:18:24 +0600

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,22 @@
+a = 10
+print(a)
+a = a + 5
+print(a)
+a = a - 2
+print(a)
+a = a * 3
+print(a)
+a = a / 2
+print(a)
+# a = a // 2
+print(a)
+a = a % 3
+print(a)
+a = a ** 2
+print('value of A is ', a)
+
+
+
+course = 'AI Lab'
+classroom = '827'
+print( f"The course is {course} and the room is {classroom}")
